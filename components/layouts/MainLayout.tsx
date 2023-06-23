56c68dfd042a06fef1bb6216c9b6f386207c7b80
Sebastian madero <rockmadero@gmail.com>
2023-06-23 12:00:05 -0500

    main layout

---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,37 @@
+import { FC, ReactNode } from 'react';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import { Sidebar } from '../ui';
+
+const inter = Inter({ subsets: ['latin'] });
+
+interface Props {
+  title: string;
+  description: string;
+  children: ReactNode;
+}
+
+export const MainLayout: FC<Props> = ({ title, description, children }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="author" content="Sebastian Madero" />
+      </Head>
+      <div className="md:flex">
+        {/* SideBar */}
+        <aside className="md:w-4/12 xl:w-1/4 2xl:w-1/5">
+          <Sidebar />
+        </aside>
+        {/* Cuerpo */}
+        <main
+          className={`${inter.className} md:w-8/12 xl:w-3/4 2xl:w-4/5 h-3/4`}
+        >
+          {children}
+        </main>
+        ;
+      </div>
+    </>
+  );
+};
