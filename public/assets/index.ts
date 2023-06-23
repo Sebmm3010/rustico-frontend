0ca99fa14f980b211f60fdd0114ebf232b83054f
Sebastian madero <rockmadero@gmail.com>
2023-06-23 15:46:53 -0500

    iconos

---
+++ public/assets/index.ts
@@ -0,0 +1,19 @@
+import alitas from './alitas.png';
+import asados from './asados.png';
+import bebidas from './bebidas.png';
+import desayuno from './desayuno.png';
+import entradas from './entradas.png';
+import familiar from './familiar.png';
+import hamburguesa from './hamburguesas.png';
+import mariscos from './mariscos.png';
+
+export {
+  alitas,
+  asados,
+  bebidas,
+  desayuno,
+  entradas,
+  familiar,
+  hamburguesa,
+  mariscos
+};
