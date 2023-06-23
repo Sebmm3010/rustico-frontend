a35c97eb300d797c8c266c0e280b3bcfaee074fd
Sebastian madero <rockmadero@gmail.com>
2023-06-23 11:59:43 -0500

    main layout y titulos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { MainLayout } from '@/components/layouts';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className="text-red-600">
+    <MainLayout
+      title="Mr. Rustico - Inicio"
+      description="Inicio de aplicacion Mr. Rustico"
+    >
       <h1>Hola mundo</h1>
-    </div>
+    </MainLayout>
   );
 }
