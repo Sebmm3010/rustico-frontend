633ebce05e236df9d69c3cd6ce76adca0c801282
Sebastian madero <rockmadero@gmail.com>
2023-06-23 10:41:02 -0500

    swr config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SWRConfig
+      value={{
+        fetcher: (resouce, init) =>
+          fetch(resouce, init).then((res) => res.json())
+      }}
+    >
+      <Component {...pageProps} />
+    </SWRConfig>
+  );
 }
