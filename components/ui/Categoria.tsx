9a4225c62e065e8d7ec7ad4566d476e4daa4fd89
Sebastian madero <rockmadero@gmail.com>
2023-06-27 16:01:39 -0500

    validando ruta en que se encuentra el ususario

--- components/ui/Categoria.tsx
+++ components/ui/Categoria.tsx
@@ -1,31 +1,26 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { ICategotias as Props } from '@/utils';
-import Link from 'next/link';
+import { UiContext } from '@/context';
 
 export const Categoria: FC<Props> = ({ name, icon, link }) => {
+  const { actualCategory, setActualCategory } = useContext(UiContext);
   const router = useRouter();
-  const onChangeCategory = (cat: string) => {
-    router.push(`/categoria/${cat}`);
-  };
+
   return (
     <div
       className={`hover:cursor-pointer  border-t-[1px] border-red-950 gap-2 p-3 hover:bg-yellow-200 ${
-        router.pathname === `/categoria/${link}` ? 'bg-yellow-200' : ''
+        actualCategory === link && router.pathname === '/categoria/[category]'
+          ? 'bg-yellow-200'
+          : ''
       }`}
     >
-      {/* <button
-        onClick={() => onChangeCategory(name.toLowerCase())}
-        className="text-xl font-bold"
-        type="button"
-        disabled={router.pathname === `/categoria/${name.toLowerCase()}`}
-        >
-        {name}
-      </button> */}
       <Link
         className="text-xl font-bold flex items-center"
         href={`/categoria/${link}`}
+        onClick={() => setActualCategory(link)}
       >
         <Image alt={name} width={75} height={75} src={icon} className="mr-5" />
         {name}
