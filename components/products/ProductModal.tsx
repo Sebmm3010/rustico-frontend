a54cf31ae03cade5c4d9fe0ab79521d6d22f3f10
Sebastian madero <rockmadero@gmail.com>
2023-07-03 15:02:10 -0500

    botones mas y menos

--- components/products/ProductModal.tsx
+++ components/products/ProductModal.tsx
@@ -7,7 +7,7 @@ import { currency } from '@/utils';
 export const ProductModal = () => {
   const { selectedProduct } = useContext(OrdersContext);
   const { toogleModal } = useContext(UiContext);
-  const { titulo, imagen, precio } = selectedProduct as IProduct;
+  const { titulo, imagen, precio, descripcion } = selectedProduct as IProduct;
   return (
     <div className="md:flex gap-10">
       <div className="md:w-1/3">
@@ -18,8 +18,8 @@ export const ProductModal = () => {
           <button type="button" onClick={toogleModal}>
             <svg
               viewBox="0 0 32 32"
-              width="25"
-              height="25"
+              width="20"
+              height="20"
               stroke="currentColor"
               fill="currentColor"
             >
@@ -27,10 +27,50 @@ export const ProductModal = () => {
             </svg>
           </button>
         </div>
-        <h1 className="text-3xl font-bold mt-5">{titulo}</h1>
+        <h1 className="text-3xl font-bold">{titulo}</h1>
+        <p className="mt-2">{descripcion}</p>
         <p className="mt-5 font-bold text-5xl text-red-950">
           {currency.format(precio)}
         </p>
+
+        {/* Menos y mas */}
+        <div className="flex gap-4 mt-5">
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+
+          <p className="text-3xl">1</p>
+
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
