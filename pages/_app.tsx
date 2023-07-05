bf18b2d08600f6cf8a2e85b76c3497aba487173b
Sebastian madero <rockmadero@gmail.com>
2023-07-05 11:27:17 -0500

    next auth initals

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,24 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { OrdersProvider, UiProvider } from '@/context';
 import { SWRConfig } from 'swr';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: (resouce, init) =>
-          fetch(resouce, init).then((res) => res.json())
-      }}
-    >
-      <OrdersProvider>
-        <UiProvider>
-          <Component {...pageProps} />
-        </UiProvider>
-      </OrdersProvider>
-    </SWRConfig>
+    <SessionProvider>
+      <SWRConfig
+        value={{
+          fetcher: (resouce, init) =>
+            fetch(resouce, init).then((res) => res.json())
+        }}
+      >
+        <OrdersProvider>
+          <UiProvider>
+            <Component {...pageProps} />
+          </UiProvider>
+        </OrdersProvider>
+      </SWRConfig>
+    </SessionProvider>
   );
 }
