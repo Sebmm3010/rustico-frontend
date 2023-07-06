773852ecf250903870b7e5da8d35db8a49e4fa3f
Sebastian madero <rockmadero@gmail.com>
2023-07-06 13:40:46 -0500

    reset auth

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,7 +1,6 @@
 import { FC, useReducer, ReactNode, useEffect } from 'react';
 import { AuthContext, authReducer } from './';
 import { IUser } from '@/interfaces';
-import { signIn, signOut, useSession } from 'next-auth/react';
 
 export interface AuthState {
   isLogged: boolean;
@@ -18,7 +17,6 @@ interface Props {
 }
 export const AuthProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, Auth_INITIAL_STATE);
-  const { data, status } = useSession();
   return (
     <AuthContext.Provider
       value={{
