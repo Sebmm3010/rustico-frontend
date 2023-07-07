7226a252726e6b36a9535042a1070737648b6835
Sebastian madero <rockmadero@gmail.com>
2023-07-07 13:53:37 -0500

    fetch error solved

--- next.config.js
+++ next.config.js
@@ -3,15 +3,15 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['res.cloudinary.com']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://rustico-api.onrender.com/:path*',
-      },
-    ]
-  },
+  }
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://rustico-api.onrender.com/:path*',
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
