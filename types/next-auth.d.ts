696a62d770d1137762a0b0a2d112b10c2961ded2
Sebastian madero <rockmadero@gmail.com>
2023-07-07 15:34:15 -0500

    accestoken for session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,6 +2,9 @@ import NextAuth from 'next-auth';
 import { IUser } from '@/interfaces';
 
 declare module 'next-auth' {
+  interface DefaultSession {
+    accessToken?: any;
+  }
   interface Session {
     user: IUser;
   }
