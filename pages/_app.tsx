d507cabc8c42a06e0306686559e19e2e39814934
Sebastian madero <rockmadero@gmail.com>
2023-07-07 10:11:59 -0500

    reset auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,23 @@
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
 import { AuthProvider, OrdersProvider, UiProvider } from '@/context';
 import { SWRConfig } from 'swr';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider>
-      <SWRConfig
-        value={{
-          fetcher: (resouce, init) =>
-            fetch(resouce, init).then((res) => res.json())
-        }}
-      >
-        <AuthProvider>
-          <OrdersProvider>
-            <UiProvider>
-              <Component {...pageProps} />
-            </UiProvider>
-          </OrdersProvider>
-        </AuthProvider>
-      </SWRConfig>
-    </SessionProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resouce, init) =>
+          fetch(resouce, init).then((res) => res.json())
+      }}
+    >
+      <AuthProvider>
+        <OrdersProvider>
+          <UiProvider>
+            <Component {...pageProps} />
+          </UiProvider>
+        </OrdersProvider>
+      </AuthProvider>
+    </SWRConfig>
   );
 }
