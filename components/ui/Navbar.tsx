b0d945fc00e63fb8c99aeb3e21b4e3fa149de416
Sebastian madero <rockmadero@gmail.com>
2023-07-13 11:55:48 -0500

    cambios menores

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,7 +1,7 @@
 import { AuthContext } from '@/context';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useContext, useState } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import { BiUserCircle, BiSearchAlt } from 'react-icons/bi';
 import { UserMenu } from './UserMenu';
 
@@ -9,28 +9,23 @@ export const Navbar = () => {
   const router = useRouter();
   const { isLogged } = useContext(AuthContext);
   const [searchInput, setSearchInput] = useState('');
-  const onSearch = () => {
+  const handleSearch = (event?: FormEvent) => {
+    event?.preventDefault();
     if (searchInput.trim().length === 0) return;
     router.push(`/buscar/${searchInput}`);
   };
   return (
-    <div className="flex items-center justify-end w-full px-7 gap-2">
+    <div className="flex items-center justify-end w-full px-7 gap-2 border-b border-red-950">
       <div className="flex items-center gap-1 my-1">
-        <input
-          type="text"
-          placeholder="Buscar"
-          className="p-1 rounded-md border border-black"
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          onKeyDown={(e) => e.key === 'Enter' && onSearch()}
-        />
-        <button
-          className="border border-black rounded-md p-1 bg-white"
-          type="button"
-          onClick={onSearch}
-        >
-          <BiSearchAlt className="text-xl" />
-        </button>
+        <form onSubmit={(e) => handleSearch(e)}>
+          <input
+            type="text"
+            placeholder="Buscar"
+            className="p-1 rounded-md border border-black"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </form>
         {isLogged ? (
           <UserMenu />
         ) : (
