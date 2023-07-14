f93889a43f8d98a413863635024dc30836182227
Sebastian madero <rockmadero@gmail.com>
2023-07-14 11:13:15 -0500

    cambios en estilo

--- components/ui/UserMenu.tsx
+++ components/ui/UserMenu.tsx
@@ -13,26 +13,31 @@ export const UserMenu: FC = () => {
       <div className="bg-gray-600/50 min-h-screen w-full fixed top-0 left-0 right-0 backdrop-blur-sm"></div>
       <div
         ref={ref}
-        className="bg-gray-500 min-h-screen w-80 fixed top-0 right-0 border-l border-white shadow-lg"
+        className="bg-gray-900 min-h-screen w-80 fixed top-0 right-0 border-l border-white shadow-lg"
       >
-        <ul className="pt-3 text-white">
-          {user?.roles.includes('admin') && (
-            <>
-              <li className="text-xl gap-2 mb-3 shadow-sm lg:hover:bg-gray-400 text-center flex justify-center items-center cursor-pointer">
-                Panel de administracion
-              </li>
-              <div className="h-[1px] bg-white w-full mb-2"></div>
-            </>
-          )}
-          <li
-            className="text-xl gap-2 mb-3 shadow-sm lg:hover:bg-gray-400 text-center flex justify-center items-center cursor-pointer"
-            onClick={() => signOut()}
-          >
-            <ImExit />
-            Salir
-          </li>
-          <div className="h-[1px] bg-white w-full"></div>
-        </ul>
+        <h1 className="text-white text-4xl font-bold text-center mt-5 border-b p-2">
+          Mr. Rustico
+        </h1>
+        <div className="flex justify-center items-center h-full left-0 right-0 absolute">
+          <ul className="p-3 text-white">
+            {user?.roles.includes('admin') && (
+              <>
+                <li className="text-xl gap-2 mb-3 shadow-sm lg:hover:bg-gray-800 text-center flex justify-center items-center cursor-pointer border-t border-b">
+                  <p className="block m-2">Panel de administracion</p>
+                </li>
+              </>
+            )}
+            <li
+              className="text-xl shadow-sm lg:hover:bg-gray-800 text-center cursor-pointer border-b"
+              onClick={() => signOut()}
+            >
+              <p className="flex justify-center items-center gap-2 p-3">
+                <ImExit />
+                Salir
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
   );
