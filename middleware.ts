42e05a0eb5473a7c69e7229fcb2fd61774852a76
Sebastian madero <rockmadero@gmail.com>
2023-07-16 16:45:22 -0500

    protegiendo rutas middleware

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+export async function middleware(req: NextRequest) {
+  const session = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET
+  });
+  const requestedPage = req.nextUrl.pathname;
+  if (!session) {
+    const url = req.nextUrl.clone();
+    url.pathname = '/auth/login';
+    return NextResponse.redirect(url);
+  }
+}
+export const config = {
+  matcher: ['/resumen/:path*', '/total/:path*']
+};
