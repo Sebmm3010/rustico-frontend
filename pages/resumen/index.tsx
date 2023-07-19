9812c329ce8c00e323c80051e099785315821cf7
Sebastian madero <rockmadero@gmail.com>
2023-07-19 12:09:29 -0500

    boton confirmar orden

--- pages/resumen/index.tsx
+++ pages/resumen/index.tsx
@@ -1,9 +1,11 @@
+import { useRouter } from 'next/router';
+import { useAppContext } from '@/hooks';
 import { MainLayout } from '@/components/layouts';
 import { OrderItemCard } from '@/components/orders';
-import { useAppContext } from '@/hooks';
 
 const ResumenPage = () => {
   const { orderItems } = useAppContext();
+  const router = useRouter();
   return (
     <MainLayout
       title="Mr. Rustico - Resumen de la orden"
@@ -16,7 +18,20 @@ const ResumenPage = () => {
           No hay elementos en la orden
         </p>
       ) : (
-        orderItems.map((item) => <OrderItemCard key={item.id} item={item} />)
+        <div>
+          {orderItems.map((item) => (
+            <OrderItemCard key={item.id} item={item} />
+          ))}
+          <div className="bg-gray-100 flex justify-end items-center rounded-lg p-4 m-2">
+            <button
+              type="button"
+              className="font-bold bg-red-950 text-white rounded-lg p-3"
+              onClick={() => router.push('/total')}
+            >
+              Continuar
+            </button>
+          </div>
+        </div>
       )}
     </MainLayout>
   );
