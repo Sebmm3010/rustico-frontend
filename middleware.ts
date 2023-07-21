5366b6188c9ef0ebf0627d7ef21d3a46ed71b4f1
Sebastian madero <rockmadero@gmail.com>
2023-07-21 17:45:24 -0500

    protegiendo cocina

--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,5 @@ export async function middleware(req: NextRequest) {
   }
 }
 export const config = {
-  matcher: ['/resumen/:path*', '/total/:path*']
+  matcher: ['/resumen/:path*', '/total/:path*', '/cocina/:path*']
 };
