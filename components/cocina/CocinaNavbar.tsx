63e1f233a9746590e3e79d406144be76f5d147cf
Sebastian madero <rockmadero@gmail.com>
2023-07-21 13:28:13 -0500

    pagina para cocina

---
+++ components/cocina/CocinaNavbar.tsx
@@ -0,0 +1,27 @@
+import { BiUserCircle } from 'react-icons/bi';
+import { useAppContext } from '@/hooks';
+import Link from 'next/link';
+import { UserMenu } from '../ui';
+
+export const CocinaNavbar = () => {
+  const { user, isLogged, setShowUserMenu, showUserMenu } = useAppContext();
+  return (
+    <div className="bg-yellow-300 fixed top-0 left-0 right-0 p-3 shadow-sm shadow-gray-200">
+      <div className="flex justify-around w-full gap-96">
+        <h1 className="font-bold text-red-900 text-xl">
+          <Link href="/cocina">Mr. Rustico - Cocina</Link>
+        </h1>
+        {!isLogged ? (
+          <button>Iniciar</button>
+        ) : (
+          <>
+            <button className="text-3xl" onClick={() => setShowUserMenu(true)}>
+              <BiUserCircle />
+            </button>
+            {showUserMenu && <UserMenu />}
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ pages/cocina/index.tsx
@@ -0,0 +1,15 @@
+import { CocinaLayout } from '@/components/layouts';
+import React from 'react';
+
+const CocinaPage = () => {
+  return (
+    <CocinaLayout
+      title="Mr. Rustico - Cocina"
+      description="Mr. Rustico pagina de cocina"
+    >
+      <h1>Cocina</h1>
+    </CocinaLayout>
+  );
+};
+
+export default CocinaPage;
