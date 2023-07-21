09a8463aead242f27ff9e5d0fb251b5959d00e8d
Sebastian madero <rockmadero@gmail.com>
2023-07-21 17:46:54 -0500

    cambiando enpoint

--- hooks/useOrders.ts
+++ hooks/useOrders.ts
@@ -3,7 +3,7 @@ import useSWR, { SWRConfiguration } from 'swr';
 
 export const useOrders = (config: SWRConfiguration = {}) => {
   const { data, error, isLoading } = useSWR<IFullOrder[]>(
-    `${process.env.NEXT_PUBLIC_API}/orders`,
+    `${process.env.NEXT_PUBLIC_API}/orders/pending`,
     config
   );
 
