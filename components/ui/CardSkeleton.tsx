56108699ab14e430a791e4d0774ae74065a8377f
Sebastian madero <rockmadero@gmail.com>
2023-07-21 14:18:28 -0500

    cambios en estilo

--- components/ui/CardSkeleton.tsx
+++ components/ui/CardSkeleton.tsx
@@ -2,11 +2,11 @@ export const CardSkeleton = () => {
   return (
     <div className="p-4 w-full bg-white rounded-lg overflow-hidden shadow hover:shadow-md">
       <div className="animate-pulse flex flex-col">
-        <div className="rounded w-full h-52 bg-gray-200"></div>
+        <div className="rounded w-full h-52 bg-gray-400"></div>
         <div className="flex flex-col mt-5">
-          <div className="w-full h-5 bg-gray-200 rounded"></div>
-          <div className="mt-2 w-10/12 h-3 bg-gray-200 rounded"></div>
-          <div className="mt-2 w-8/12 h-3 bg-gray-200 rounded"></div>
+          <div className="w-full h-5 bg-gray-400 rounded"></div>
+          <div className="mt-2 w-10/12 h-3 bg-gray-400 rounded"></div>
+          <div className="mt-2 w-8/12 h-3 bg-gray-400 rounded"></div>
         </div>
       </div>
     </div>
