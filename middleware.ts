766942d65100df76fa0fe3fe6e3784fb6dd398d2
Sebastian madero <rockmadero@gmail.com>
2023-07-25 10:33:05 -0500

    proteccion de rutas admin y guardar pagina anterior

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 export async function middleware(req: NextRequest) {
-  const session = await getToken({
+  const session: any = await getToken({
     req,
     secret: process.env.NEXTAUTH_SECRET
   });
@@ -10,8 +10,24 @@ export async function middleware(req: NextRequest) {
   if (!session) {
     const url = req.nextUrl.clone();
     url.pathname = '/auth/login';
+    url.search = `p=${requestedPage}`;
+    if (requestedPage.includes('api')) {
+      return new Response(JSON.stringify({ msg: 'No esta autorizado' }), {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    }
     return NextResponse.redirect(url);
   }
+  // if (
+  //   requestedPage.includes('/admin') &&
+  //   !session.user.roles.includes('admin')
+  // ) {
+  //   return NextResponse.redirect(new URL('/', req.url));
+  // }
+  // return NextResponse.next();
 }
 export const config = {
   matcher: ['/resumen/:path*', '/total/:path*', '/cocina/:path*']
